fe396f25c4c6b0e38c0ca243a71b8d9ffbb0ef54
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-10-29 15:35:43 +0100

    Update about.py

--- src/utils/about.py
+++ src/utils/about.py
@@ -6,7 +6,7 @@ __description__ = ' '.join('''
 Paulissoft Application Tools for Oracle (PATO) GUI.
 '''.strip().split())
 __email__ = "paulissoft@gmail.com"
-__version_info__ = ('3', '1', '0')
+__version_info__ = ('3', '2', '0')
 __version__ = '.'.join(__version_info__)
 __license__ = "MIT License"
 __copyright__ = 'Copyright (c) 2021-2023 Gert-Jan Paulissen'
