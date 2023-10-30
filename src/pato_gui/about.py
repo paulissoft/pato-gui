a7c6fe5faeb4b420b7a36ac206a0d8905806fd0c
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2023-10-30 13:37:58 +0100

    Changed about.

--- src/pato_gui/about.py
+++ src/pato_gui/about.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
-__title__ = 'Paulissoft Application Tools for Oracle (PATO) GUI'
-__package_name__ = 'pato-gui'
-__author__ = "Gert-Jan Paulissen"
-__description__ = ' '.join('''
-Paulissoft Application Tools for Oracle (PATO) GUI.
-'''.strip().split())
-__email__ = "paulissoft@gmail.com"
-__version_info__ = ('3', '2', '0')
-__version__ = '.'.join(__version_info__)
-__license__ = "MIT License"
+
+import sys
+try:
+    from importlib import metadata as importlib_metadata
+except ImportError:
+    # Backwards compatibility - importlib.metadata was added in Python 3.8
+    import importlib_metadata
+
+
+# Find the name of the module that was used to start the app
+app_module = sys.modules['__main__'].__package__
+# Retrieve the app's metadata
+metadata = importlib_metadata.metadata(app_module)
+
+__title__ = metadata['Formal-Name']
+# __package_name__ = 'pato-gui'
+__author__ = metadata['Author']
+__description__ = metadata['Summary']
+__email__ = metadata['Author-email']
+__version__ = '3.2.0'
+__version_info__ = tuple(__version__.split("."))
+__license__ = 'MIT License'
 __copyright__ = 'Copyright (c) 2021-2023 Gert-Jan Paulissen'
-__url__ = "https://github.com/paulissoft/pato-gui"
+__url__ = metadata['Home-page']
 __help_url__ = "https://paulissoft.github.io/pato-gui"
 
 if __name__ == '__main__':
